c61fb0dad57c1c52426e98ab7b657996d010e598
amanmehrotra13 <amanpankaj.mehrotra@gmail.com>
2026-02-10 04:06:16 +0530

    Update silver.sql

--- transformations/silver.sql
+++ transformations/silver.sql
@@ -15,18 +15,19 @@ SEQUENCE BY ingestion_timestamp
 STORED AS SCD TYPE 2;
 
 
-CREATE OR REFRESH MATERIALIZED VIEW city_travels_medallion.b_silver.silver_trips(
-CONSTRAINT valid_distance EXPECT (distance_travelled_km > 0) ON VIOLATION DROP ROW,
-CONSTRAINT valid_trip_id EXPECT (trip_id IS NOT NULL) ON VIOLATION FAIL UPDATE
+CREATE OR REFRESH STREAMING TABLE city_travels_medallion.b_silver.silver_trips (
+  CONSTRAINT valid_distance EXPECT (distance_travelled_km > 0) ON VIOLATION DROP ROW,
+  CONSTRAINT valid_trip_id EXPECT (trip_id IS NOT NULL) ON VIOLATION FAIL UPDATE
 )
-AS
-SELECT 
-trip_id, 
-t.date as trip_date,
-city_id,
-passenger_type,
-CAST(distance_travelled_km AS INT) AS distance_travelled_km,
-fare_amount,
-passenger_rating,
-driver_rating
-FROM city_travels_medallion.a_bronze.bronze_trips t;
+AS SELECT 
+  trip_id, 
+  CAST(t.date AS DATE) as trip_date, -- Ensure proper Date type
+  city_id,
+  TRIM(UPPER(passenger_type)) AS passenger_type,
+  CAST(distance_travelled_km AS DOUBLE) AS distance_travelled_km,
+  fare_amount,
+  ROUND((fare_amount / distance_travelled_km), 2) AS cost_per_km,
+  DATE_FORMAT(t.date, 'EEEE') AS day_of_week,
+  COALESCE(passenger_rating, 0) AS passenger_rating,
+  COALESCE(driver_rating, 0) AS driver_rating 
+FROM STREAM(city_travels_medallion.a_bronze.bronze_trips) t;
